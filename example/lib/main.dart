1dab76562f651dc7bf28e3c6628f6356ce5f097d
Ghous Muhammad <ghousmuhammad2016@gmail.com>
2024-02-14 13:31:31 +0500

    updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue_background/flutter_blue_background.dart';
 import 'package:flutter_blue_background_example/permissions/bluetooth_adapter.dart';
 import 'package:flutter_blue_background_example/permissions/check_status.dart';
-import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -19,44 +18,12 @@ class _MyAppState extends State<MyApp> {
 
   String readData = "";
 
-  // @override
-  // void dispose() {
-  //   WidgetsBinding.instance.removeObserver(this);
-  //   super.dispose();
-  // }
-  //
-  // @override
-  // void didChangeAppLifecycleState(AppLifecycleState state) {
-  //   if (state == AppLifecycleState.paused) {
-  //     // Your code to execute when the app is minimized
-  //     IosBackPlugin.executeInBackground(() {
-  //       Timer.periodic(Duration(seconds: 15), (timer) async {
-  //         print('execute function after 5 sec ');
-  //         final flutterReactiveBle = FlutterReactiveBle();
-  //         flutterReactiveBle.scanForDevices(withServices: [], scanMode: ScanMode.lowLatency).listen((device) {
-  //           print('scan device is ${device.name}');
-  //           if(device.name == "grotz testing-"){
-  //             print('device find');
-  //           }
-  //           //code for handling results
-  //         }, onError: (e) {
-  //           print('error while scanning $e');
-  //           //code for handling error
-  //         });
-  //
-  //       });
-  //       print("Executing function in the background");
-  //     });
-  //   }
-  // }
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Flutter Blue Background Example'),
         ),
         body: Center(
           child: Column(
@@ -99,44 +66,6 @@ class _MyAppState extends State<MyApp> {
                   child: const Text('Start BG Android Service')
               ),
               const SizedBox(height: 20,),
-              ElevatedButton(onPressed: () async {
-                // Ios Background Work below
-
-                await FlutterBlueBackground.startFlutterBackgroundService(() {
-
-                  Timer.periodic(const Duration(seconds: 8), (timer) async {
-                    await FlutterBlueBackground.connectToDevice(
-                        deviceName: 'Esp32',
-                        serviceUuid: '4fafc201-1fb5-459e-8fcc-c5c9c331914b',
-                        characteristicUuid: 'beb5483e-36e1-4688-b7f5-ea07361b26a8'
-                    );
-
-                    // Write value on specific characteristic
-                    await FlutterBlueBackground.writeData(
-                        serviceUuid: '4fafc201-1fb5-459e-8fcc-c5c9c331914b',
-                        characteristicUuid: 'beb5483e-36e1-4688-b7f5-ea07361b26a8',
-                        data: 'something'
-                    );
-
-                    String? data = await FlutterBlueBackground.readData(
-                        serviceUuid: '4fafc201-1fb5-459e-8fcc-c5c9c331914b',
-                        characteristicUuid: 'beb5483e-36e1-4688-b7f5-ea07361b26a8'
-                    );
-                    // print("data in main is $data");
-                  });
-
-                  print("Executing function in the background");
-                });
-
-
-              },
-                  child: const Text('Start BG IOS Service')
-              ),
-              const SizedBox(height: 30,),
-              Text("Read value is $readData" ,
-              style: const TextStyle(
-                fontSize: 22
-              ),),
               const SizedBox(height: 20,),
               ElevatedButton(
                   onPressed: () async {
@@ -146,14 +75,14 @@ class _MyAppState extends State<MyApp> {
               const SizedBox(height: 20,),
               ElevatedButton(
                   onPressed: () async {
-                    await FlutterBlueBackground.clearReadStorage();
+                    // This method will get all the read data
+                    await FlutterBlueBackground.getReadDataAndroid();
                   }, child: const Text('Clear Read Data List')
               ),
-
               const SizedBox(height: 20,),
               ElevatedButton(
                   onPressed: () async {
-                    await FlutterBlueBackground.getReadDataAndroid();
+                    await FlutterBlueBackground.clearReadStorage();
                   }, child: const Text('Clear Read Data List')
               ),
 
